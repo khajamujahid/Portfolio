59a4b9b7a47bc8d41735744659a47818d2a83fdf
Khajamujahid <165094055+khajamujahid@users.noreply.github.com>
2026-09-11 13:48:08 -0700

    Revert redesign: restore next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "source.unsplash.com" },
-      { protocol: "https", hostname: "images.unsplash.com" }
-    ]
-  }
+  images: { remotePatterns: [ { protocol: 'https', hostname: 'source.unsplash.com' } ] }
 };
 module.exports = nextConfig;
